220955d9dde95a9eb0eb7d009aba3ceaf07bcf08
Gabe Jacobs <GabeJacobs@users.noreply.github.com>
2026-05-14 13:12:21 -0300

    1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = resolvedVersionCode
-        versionName = "1.1.4"
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
